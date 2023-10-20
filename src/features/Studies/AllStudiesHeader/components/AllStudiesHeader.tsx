0f30993a46c953e79b654ff61a94a63456c497ec
contr.dmitry.sidoruk <contr.dmitry.sidoruk@pandadoc.com>
2023-10-20 10:35:12 +0300

    Add all studies controls

--- src/features/Studies/AllStudiesHeader/components/AllStudiesHeader.tsx
+++ src/features/Studies/AllStudiesHeader/components/AllStudiesHeader.tsx
@@ -1,5 +1,16 @@
+import './styles.scss';
+
 const AllStudiesHeader = () => {
-  return <h1>All Studies</h1>;
+  return (
+    <>
+      <h1>All Studies</h1>
+      <div className="contorls">
+        <button type="button">Add Study</button>
+        <button type="button">Add Study Set</button>
+        <button type="button">Show Editable Studies Only</button>
+      </div>
+    </>
+  );
 };
 
 export default AllStudiesHeader;
